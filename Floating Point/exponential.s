288cc613d0295471b8cc781491e5262390faf0e9
Nihar Desai <nihar.desai1@gmail.com>
2017-12-01 23:42:51 +0530

    Update exponential.s

--- Floating Point/exponential.s
+++ Floating Point/exponential.s
@@ -1,5 +1,5 @@
 ;calculate e^x in assembly
-;using series e^x = 1 + 2^x/2! + 3^x/3! + ...
+;using series e^x = 1 + + x + x^2/2! + x^3/3! + ...
 
  	 AREA     arm_seembly, CODE, READONLY
      EXPORT __main
@@ -10,21 +10,21 @@ __main  FUNCTION
 exp	SN s0	;exp in s0
 n	SN s1	;up to n terms
 sum SN s2	; cumulative sum
-temp SN s3
+temp SN s3	
 i sN    s4	;iterations
 
 
-	VMOV.F32 exp,#3	;
-	VMOV.F32 n,#10	
-	VMOV.F32 temp,#1	
+	VMOV.F32 exp,#3		;count for e^x
+	VMOV.F32 n,#10		; upto 10 terms
+	VMOV.F32 temp,#1	;
 	VMOV.F32 n,#1
 	VMOV.F32 sum,#1
 	
 	
 series
 	VCMP.F32 n,i					;check for i <= n	
-	VMRS.F32 APSR_nzcv,fpscr		;copy flags from fpscr to apsr before testing eq
-	BEQ stop						
+	VMRS.F32 APSR_nzcv,fpscr		;copy flags from fpscr to apsr
+	BEQ stop				;terminate if i == n		
 	VMUL.F32 temp,temp,exp			; temp = temp*exp
 	VDIV.F32 temp,temp,i			; temp = temp/i
 	VADD.F32 sum,sum,temp			; sum = sum+temp
