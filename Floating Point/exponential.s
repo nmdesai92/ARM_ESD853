bf01b9fb02dde974ff012d26d872a9d9d3321946
Nihar Desai <nihar.desai1@gmail.com>
2017-12-01 23:38:42 +0530

    Update exponential.s

--- Floating Point/exponential.s
+++ Floating Point/exponential.s
@@ -1,34 +1,35 @@
 ;calculate e^x in assembly
+;using series e^x = 1 + 2^x/2! + 3^x/3! + ...
+
  	 AREA     arm_seembly, CODE, READONLY
      EXPORT __main
 	 ENTRY 
 
 
 __main  FUNCTION	
-x SN 1
-result SN 3	;result
-temp SN 2	;temporary var
-n SN 4		;no of iterations
-count SN 5
-one SN 6
+exp	SN s0	;exp in s0
+n	SN s1	;up to n terms
+sum SN s2	; cumulative sum
+temp SN s3
+i sN    s4	;iterations
 
-	VMOV.F32 x,#3	;
-	VMOV.F32 n,#10	;up to 10 terms
-	VMOV.F32 one,#1
+
+	VMOV.F32 exp,#3	;
+	VMOV.F32 n,#10	
 	VMOV.F32 temp,#1	
-	VMOV.F32 count,#1
-	VMOV.F32 result,#1
+	VMOV.F32 n,#1
+	VMOV.F32 sum,#1
 	
 	
-loop
-	VCMP.F32 n,count		;loop till n reaches two accounting
-	VMRS.F32 APSR_nzcv,fpscr;copy flags from fpscr to apsr before testing eq
-	BEQ stop
-	VMUL.F32 temp,temp,x	
-	VDIV.F32 temp,temp,count
-	VADD.F32 result,result,temp
-	VADD.F32 count,count,one
-	B loop
+series
+	VCMP.F32 n,i					;check for i <= n	
+	VMRS.F32 APSR_nzcv,fpscr		;copy flags from fpscr to apsr before testing eq
+	BEQ stop						
+	VMUL.F32 temp,temp,exp			; temp = temp*exp
+	VDIV.F32 temp,temp,i			; temp = temp/i
+	VADD.F32 sum,sum,temp			; sum = sum+temp
+	VADD.F32 i,i,#1					;i++
+	B series
     
 stop B stop ; stop program
      ENDFUNC
